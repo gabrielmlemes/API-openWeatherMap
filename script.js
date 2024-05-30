cf9b3f6d7446e29eca05f35ef2739e5bf5d1e051
Gabriel Lemes <g.moreiralemess@gmail.com>
2024-05-30 13:11:48 -0300

    initial commit

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 const key = 'dc8a2568cf6a485deded6c69b890f087';
+const cidade = document.querySelector("#cidade")
+const content = document.querySelector(".content")
+const graus = document.querySelector("#graus")
+const previsao = document.querySelector("#previsao")
+const tempo = document.querySelector("#tempo")
+const imgPrevisao = document.querySelector("#img-previsao")
 
 // 3-Função para colocar os dados na tela
 function colocarDadosNaTela(dados) {
-    console.log(dados);
-    const content = document.querySelector(".content")
     content.style.display = 'block'
-    document.querySelector("#cidade").innerHTML = `Tempo em ${dados.name}`
-    document.querySelector("#graus").innerHTML = Math.floor(dados.main.temp) + '°C'
-    document.querySelector("#previsao").innerHTML = dados.weather[0].description
-    document.querySelector("#tempo").innerHTML = `Umidade: ${dados.main.humidity}%`
-    document.querySelector("#img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+
+    if (dados.name === undefined) {
+        cidade.textContent = 'Local inválido'
+        graus.style.display = 'none'
+        previsao.style.display = 'none'
+        tempo.style.display = 'none'
+        imgPrevisao.style.display = 'none'
+    } else {
+        cidade.innerHTML = `Tempo em ${dados.name}`
+        graus.innerHTML = Math.floor(dados.main.temp) + '°C'
+        previsao.innerHTML = dados.weather[0].description
+        tempo.innerHTML = `Umidade: ${dados.main.humidity}%`
+        imgPrevisao.src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+    }
+
 }
 
 // 2-Função para acessar o servidor do OpenWeather:
 
 async function buscarCidade(cidade) {
 
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`)
+    .then(res => res.json())
 
     colocarDadosNaTela(dados)
 }
